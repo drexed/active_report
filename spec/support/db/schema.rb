4641c4b1dedfb872e9cf80233746cef73ebc58c2
Juan Gomez <j.gomez@drexed.com>
2016-06-02 16:32:34 -0400

    updates

--- spec/support/db/schema.rb
+++ spec/support/db/schema.rb
@@ -1,9 +1,9 @@
 ActiveRecord::Schema.define(version: 1) do
   create_table :cars, force: :cascade do |t|
-    t.string  :name
+    t.string :name
     t.integer :speed
     t.integer :hp
     t.boolean :crash_safety_rated
-    t.string  :created_at
+    t.string :created_at
   end
 end
